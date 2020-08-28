5320c366d3c62b61cca6a33bee8ce71c994ec2bc
Pahud <pahudnet@gmail.com>
2020-08-28 09:18:44 +0800

    feat(core): ConstructLibraryAws API

--- .projenrc.js
+++ .projenrc.js
@@ -1,74 +1,53 @@
-const { JsiiProject, Semver } = require('projen');
+const {
+  ConstructLibraryAws
+} = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.55.0';
+const AWS_CDK_LATEST_RELEASE = '1.61.0';
 const PROJECT_NAME = 'cdk-elasticache-monitor';
 const PROJECT_DESCRIPTION = 'ElasticacheAutoMonitor allows you to send email, sms, slack, or trigger aws lambda when an alarm occurs.';
 
-const project = new JsiiProject({
-  "name": PROJECT_NAME,
+const project = new ConstructLibraryAws({
+  name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
-  "authorName": "Jiale Chan",
-  "authorEmail": "jiale.chan@gmail.com",
-  "repository": "https://github.com/jialechan/cdk-elasticache-monitor.git",
+  authorName: "Jiale Chan",
+  authorEmail: "jiale.chan@gmail.com",
+  repository: "https://github.com/jialechan/cdk-elasticache-monitor.git",
   stability: 'experimental',
-  devDependencies: {
-    '@aws-cdk/assert': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@types/jest': Semver.caret('26.2.1'),
-    '@types/node': Semver.caret('14.5.0'),
-  },
-  peerDependencies: {
-    constructs: Semver.caret('3.0.4'),
-    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-cloudwatch': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-cloudwatch-actions': Semver.caret(AWS_CDK_LATEST_RELEASE),
-  },
-  dependencies: {
-    constructs: Semver.caret('3.0.4'),
-    '@aws-cdk/core': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-cloudwatch': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-sns-subscriptions': Semver.caret(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-cloudwatch-actions': Semver.caret(AWS_CDK_LATEST_RELEASE),
-  },
-  python: {
-    distName: 'cdk-elasticache-monitor',
-    module: 'cdk-elasticache-monitor'
-  }
-});
 
-project.addFields({
-  'keywords': [
+  keywords: [
     'cdk',
     'aws',
     'elasticache',
     'monitor',
     'alarm',
     'slack',
-  ]
-});
+  ],
 
-project.addFields({
-  'awscdkio': {
-    'twitter': '@jialechan',
-    'announce': false,
+  catalog: {
+    twitter: 'jialechan',
+    announce: false,
+  },
+
+  // creates PRs for projen upgrades
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  cdkVersion: AWS_CDK_LATEST_RELEASE,
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-cloudwatch',
+    '@aws-cdk/aws-sns',
+    '@aws-cdk/aws-sns-subscriptions',
+    '@aws-cdk/aws-cloudwatch-actions',
+  ],
+
+  python: {
+    distName: 'cdk-elasticache-monitor',
+    module: 'cdk-elasticache-monitor'
   }
 });
 
-project.gitignore.exclude(
-  'cdk.context.json',
-  'cdk.out',
-);
-
-project.npmignore.exclude(
-  'cdk.context.json',
-  'cdk.out',
-  'coverage',
-);
-
-project.synth();
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
 
 project.synth();
